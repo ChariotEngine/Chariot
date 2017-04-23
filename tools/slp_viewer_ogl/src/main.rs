4e79aa3c8662039e10a69f5298743559e3ff4bf1
Taryn Hill <taryn@phrohdoh.com>
2017-04-23 07:02:28 -0500

    Initial commit of slp-viewer-ogl

--- tools/slp_viewer_ogl/src/main.rs
+++ tools/slp_viewer_ogl/src/main.rs
@@ -1,3 +1,39 @@
+#[macro_use]
+extern crate glium;
+
+extern crate glium_sdl2;
+extern crate sdl2;
+
+use glium_sdl2::DisplayBuild;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let display = video_subsystem.window("My window", 800, 600)
+        .resizable()
+        .build_glium()
+        .unwrap();
+
+    let mut running = true;
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    while running {
+        let mut target = display.draw();
+        // do drawing here...
+        target.finish().unwrap();
+
+        // Event loop: includes all windows
+
+        for event in event_pump.poll_iter() {
+            use sdl2::event::Event;
+
+            match event {
+                Event::Quit { .. } => {
+                    running = false;
+                }
+                _ => (),
+            }
+        }
+    }
 }
